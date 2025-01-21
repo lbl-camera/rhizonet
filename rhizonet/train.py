b896a4b71086b2f439ceebc5113846add5af7f02
Zineb-Sordo <zinebsordo@hotmail.fr>
2025-01-21 16:59:22 -0600

    shape error in predict

--- rhizonet/train.py
+++ rhizonet/train.py
@@ -106,7 +106,7 @@ def train_model(args):
 
     # Split data into training, validation and test sets
     train_len, val_len, test_len = np.cumsum(np.round(len(images) * np.array(dataset_params['data_split'])).astype(int))
-    idx = np.random.permutation(np.arange(len(images)))[:100]
+    idx = np.random.permutation(np.arange(len(images)))[:1000]
 
     train_images = [images[i] for i in idx[:train_len]]
     train_labels = [labels[i] for i in idx[:train_len]]
