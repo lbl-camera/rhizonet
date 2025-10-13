1fe57ad7947130b4b1a20ee31ff0efa398ca03a9
Zineb-Sordo <zinebsordo@hotmail.fr>
2025-10-13 16:01:52 +0200

    added matching verification in prepare patches step

--- tests/test_prepare_patches.py
+++ tests/test_prepare_patches.py
@@ -20,7 +20,7 @@ def sample_config():
         "step_size": [32, 32],
         "min_labeled": 0.1,
         "nb_pred_data": None,
-        "label_prefix": "Annotation"
+        "label_prefix": ""
     }
 
 def test_parse_prepare_variables(sample_config):
@@ -32,7 +32,7 @@ def test_parse_prepare_variables(sample_config):
         "step_size": [32, 32],
         "min_labeled": 0.1,
         "nb_pred_data": None,
-        "label_prefix": "Annotation"
+        "label_prefix": ""
     }
 
     with TemporaryDirectory() as temp_dir:
@@ -56,7 +56,7 @@ def test_prepare_patches():
         "step_size": (32, 32),
         "min_labeled": 0.1,
         "nb_pred_data": None,
-        "label_prefix": "Annotation"
+        "label_prefix": ""
     }
     with TemporaryDirectory() as temp_dir:
         images_dir = os.path.join(temp_dir, "images")
@@ -70,7 +70,7 @@ def test_prepare_patches():
             label[32:96, 32:96] = 1  # Annotated region
 
             io.imsave(os.path.join(images_dir, f"image_{i}.png"), img)
-            io.imsave(os.path.join(labels_dir, f"Annotationlabel_{i}.png"), label)
+            io.imsave(os.path.join(labels_dir, f"label_{i}.png"), label)
 
         sample_config["images_path"] = images_dir
         sample_config["labels_path"] = labels_dir
